dabce1c3db79ef5dc88642af137bb667abf9e451
MohammedAbuEssa <mabuissa5@gmail.com>
2023-10-08 21:31:04 +0000

    First push

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar/Navbar";
+import TimeDate from "./components/Home/TimeDate";
+import Announcment from "./components/Home/Announcment/Announcment";
+export default function Home() {
+  return (
+    <div className="page">
+      <Navbar />
+      <div className="grid grid-cols-1 md:grid-cols-3 shadow-md p-4 gap-2 w-full">
+        <div className="col-span-2 bg-gray-50 p-4 rounded-lg dark:bg-darkcomp">
+          <Announcment />
+        </div>
+        <TimeDate />
+      </div>
+    </div>
+  );
+}
