f6c0f4a1ad028cec9f354d6360daf6172e79a784
MohammedAbuEssa <mabuissa5@gmail.com>
2023-10-09 15:17:48 +0000

    Fix logout

--- app/components/Navbar/Logout.jsx
+++ app/components/Navbar/Logout.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import Link from 'next/link'
-import Option from './Option'
-import icons from '@/public/icons.jsx'
+import React from "react";
+import Link from "next/link";
+import Option from "./Option";
+import icons from "@/public/icons.jsx";
+import Cookies from "js-cookie";
 
-export default function Logout({route}) {
+export default function Logout({ route }) {
+  function clearAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+  }
+
   return (
-    <Option route={route} title="logout" type='logout' className="logout"/>
-  )
+    <div onClick={clearAllCookies}>
+      <Option route={route} title="logout" type="logout" className="logout" />
+    </div>
+  );
 }
